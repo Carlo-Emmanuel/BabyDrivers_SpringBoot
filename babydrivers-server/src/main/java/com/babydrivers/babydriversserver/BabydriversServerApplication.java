9cbd06c2763c284033098b23d7d49fdfb92e8d51
Kenneth Dam <mrdam.kiet@gmail.com>
2023-11-22 15:59:43 -0800

    File creation

---
+++ babydrivers-server/src/main/java/com/babydrivers/babydriversserver/BabydriversServerApplication.java
@@ -0,0 +1,13 @@
+package com.babydrivers.babydriversserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabydriversServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabydriversServerApplication.class, args);
+    }
+
+}
---
+++ babydrivers-server/src/test/java/com/babydrivers/babydriversserver/BabydriversServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babydrivers.babydriversserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabydriversServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
