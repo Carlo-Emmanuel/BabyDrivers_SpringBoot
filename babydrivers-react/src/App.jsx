0da83f0eda0569bdf097019cb414c2cad9abd2f3
MichaelMet <mikegmac14@gmail.com>
2023-11-29 18:21:53 -0800

    Added stuff

--- babydrivers-react/src/App.jsx
+++ babydrivers-react/src/App.jsx
@@ -2,13 +2,17 @@ import './App.css';
 import { NavBar } from './components/NavBar';
 import HomeBody from './components/HomeBody';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-    <NavBar></NavBar>
-    <HomeBody></HomeBody>
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar></NavBar>
+        <HomeBody></HomeBody>
+      </div>
+    </Router>
   );
 }
 
