8ef4cd4a93ef64d1a554870334f6b51badfc3fcc
MichaelMet <mikegmac14@gmail.com>
2023-12-05 12:24:41 -0800

    New components added

--- babydrivers-react/src/components/Form.jsx
+++ babydrivers-react/src/components/Form.jsx
@@ -186,6 +186,35 @@ const Form = () => {
           Create Reservation
         </button>
       </form>
+      <div class="dropdown">
+        <a
+          class="btn btn-secondary dropdown-toggle"
+          href="#"
+          role="button"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Dropdown link
+        </a>
+
+        <ul class="dropdown-menu">
+          <li>
+            <a class="dropdown-item" href="#">
+              Action
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Another action
+            </a>
+          </li>
+          <li>
+            <a class="dropdown-item" href="#">
+              Something else here
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
